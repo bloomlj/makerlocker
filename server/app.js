aed25bc183a31f45d3ae1ff34e88aa62adf389c3
bloomlj <kollocc@live.com>
2014-09-04 19:11:48 +0800

    remove no use files

---
+++ server/app.js
@@ -0,0 +1,21 @@
+var net = require('net');
+var server = net.createServer(function(c) { //'connection' listener
+  console.log('server connected');
+  c.on('end', function() {
+    console.log('server disconnected');
+  });
+  c.on('data',function(data){
+    	c.write("A");
+	console.log(c.remoteAddress + ":" + c.remotePort + ">" + data);
+        //process.stdout.write(data);
+	console.log(data.slice(0,8));
+	//cardid = data.substring(0,3);
+        //console.log(cardid);	
+      	//broadcast(data);
+  });
+  c.write('hello\r\n');
+  //c.pipe(c);
+});
+server.listen(1234, function() { //'listening' listener
+  console.log('server bound on port 1234');
+});
